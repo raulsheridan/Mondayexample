de8ecf1aeb85ed109948708beeaa073433d2b5b4
raulsheridan <desairausheidancollege.ca>
2023-05-29 17:28:21 -0400

    student project version = 1

---
+++ src/mondayexample/MondayExample.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package mondayexample;
+
+/**
+ *
+ * @author 16474
+ */
+public class MondayExample {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("learing github operation");
+        System.out.println("learing github pull");
+        System.out.println("learing github operation fetch +metrge");
+
+         
+    }
+    
+}
